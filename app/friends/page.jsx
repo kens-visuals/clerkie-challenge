6b67d5f2575712ddf56423d357cbdf7103f02c46
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-05-16 16:55:39 -0700

    add module to friends page

--- app/friends/page.jsx
+++ app/friends/page.jsx
@@ -1,5 +1,35 @@
+'use client';
+import { useState } from 'react';
+import Image from 'next/image';
+
+import Filters from '@/components/Filters';
+import ClearAllButton from '@/components/ClearAllButton';
+
 import styles from './styles.module.css';
 
+import filterIcon from '../../public/filter-icon.svg';
+
 export default function Frineds() {
-  return <h1 className={styles.heading}>Friends</h1>;
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+
+  const handleToggleFilter = () => setIsFilterOpen((prevState) => !prevState);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.buttonContainer}>
+        <button
+          type="button"
+          onClick={handleToggleFilter}
+          className={styles.filterButton}
+        >
+          <Image src={filterIcon} width={20} height={20} alt="filter" />
+        </button>
+        <div className={styles.divider} />
+
+        <ClearAllButton />
+
+        {isFilterOpen && <Filters setIsFilterOpen={setIsFilterOpen} />}
+      </div>
+    </div>
+  );
 }
