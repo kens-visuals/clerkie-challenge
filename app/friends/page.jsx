54e8dcae60b4cc718acd43ba43fad8491ae4efb9
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-05-18 14:31:23 -0700

    add filters functionality

--- app/friends/page.jsx
+++ app/friends/page.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 
 import Filters from '@/components/Filters';
 import ClearAllButton from '@/components/ClearAllButton';
+import FriendsList from '@/components/FriendsList';
 
 import styles from './styles.module.css';
 
@@ -11,25 +12,47 @@ import filterIcon from '../../public/filter-icon.svg';
 
 export default function Frineds() {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
   const handleToggleFilter = () => setIsFilterOpen((prevState) => !prevState);
 
+  const handleFilterReset = () => setSelectedOptions([]);
+
   return (
     <div className={styles.container}>
-      <div className={styles.buttonContainer}>
+      <div className={styles.filterButtonContainer}>
         <button
           type="button"
           onClick={handleToggleFilter}
-          className={styles.filterButton}
+          className={
+            isFilterOpen
+              ? `${styles.filterButton} ${styles.filterButtonActive}`
+              : `${styles.filterButton} ${styles.filterButtonDefault}`
+          }
         >
           <Image src={filterIcon} width={20} height={20} alt="filter" />
+          {selectedOptions && (
+            <span className={styles.filterCount}>{selectedOptions.length}</span>
+          )}
         </button>
         <div className={styles.divider} />
 
-        <ClearAllButton />
+        <ClearAllButton
+          selectedOptions={selectedOptions}
+          handleFilterReset={handleFilterReset}
+        />
 
-        {isFilterOpen && <Filters setIsFilterOpen={setIsFilterOpen} />}
+        {isFilterOpen && (
+          <Filters
+            selectedOptions={selectedOptions}
+            setIsFilterOpen={setIsFilterOpen}
+            handleFilterReset={handleFilterReset}
+            setSelectedOptions={setSelectedOptions}
+          />
+        )}
       </div>
+
+      <FriendsList selectedOptions={selectedOptions} />
     </div>
   );
 }
